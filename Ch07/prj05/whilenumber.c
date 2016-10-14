7e35962377929a33969b7384758ffeed047f77dd
hj1221p <hj1221p@naver.com>
2016-10-14 15:24:16 +0900

    Ch07의 prj05 수행
    whilenumber.c

---
+++ Ch07/prj05/whilenumber.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+#define MAX 5
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= MAX)
+	{
+		print("%d\n", n++);
+
+
+		print("\n제어변수 count => %d\n, n);
+
+
+			return 0;
+	}
+
